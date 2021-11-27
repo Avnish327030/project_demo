1ce58ae4ebedf480d1aff5ef0a8bf3ab4de22da7
Avnish327030 <anishyadav7045075175@gmail.com>
2021-11-27 12:34:02 +0530

    flask application initialized

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return "Hello world"
+
+if __name__ == '__main__':
+    app.run(debug=True)
