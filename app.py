484a3648d9ac77d1b4e18ee71ccf5063ef4ed9ba
Avnish327030 <anishyadav7045075175@gmail.com>
2021-11-27 12:47:19 +0530

    message has been modified

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return "Hello world"
+    return "Hello Avnish"
 
 if __name__ == '__main__':
     app.run(debug=True)
